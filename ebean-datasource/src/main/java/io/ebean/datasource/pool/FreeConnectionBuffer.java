9a8bc2dc05eb0da7732b5c848708d066fd9657d8
Roland Praml <roland.praml@foconis.de>
2025-03-25 07:53:22 +0100

    Refactored FreeConnectionBuffer to use custom linked list

--- ebean-datasource/src/main/java/io/ebean/datasource/pool/FreeConnectionBuffer.java
+++ ebean-datasource/src/main/java/io/ebean/datasource/pool/FreeConnectionBuffer.java
@@ -1,6 +1,7 @@
 package io.ebean.datasource.pool;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A buffer designed especially to hold free pooled connections.
@@ -10,45 +11,58 @@ import java.util.*;
  */
 final class FreeConnectionBuffer {
 
-  /**
-   * Buffer oriented for add and remove.
-   */
-  private final LinkedList<PooledConnection> freeBuffer = new LinkedList<>();
+
+  private final Node free = new Node(null);
+
+  FreeConnectionBuffer() {
+    Node end = new Node(null);
+    free.next = end;
+    end.prev = free;
+  }
+
+  int size = 0;
 
   /**
    * Return the number of entries in the buffer.
    */
   int size() {
-    return freeBuffer.size();
+    return size;
   }
 
   /**
    * Return true if the buffer is empty.
    */
   boolean isEmpty() {
-    return freeBuffer.isEmpty();
+    return size == 0;
   }
 
   /**
    * Add connection to the free list.
    */
   void add(PooledConnection pc) {
-    freeBuffer.addFirst(pc);
+    free.add(new Node(pc));
+    size++;
   }
 
   /**
    * Remove a connection from the free list.
    */
   PooledConnection remove() {
-    return freeBuffer.removeFirst();
+    Node node = free.next;
+    node.remove();
+    size--;
+    return node.pc;
   }
 
   /**
    * Close all connections in this buffer.
    */
   void closeAll(boolean logErrors) {
-    List<PooledConnection> tempList = new ArrayList<>(freeBuffer);
-    freeBuffer.clear();
+    List<PooledConnection> tempList = new ArrayList<>();
+    while (size > 0) {
+      tempList.add(remove());
+    }
+
     if (Log.isLoggable(System.Logger.Level.TRACE)) {
       Log.trace("... closing all {0} connections from the free list with logErrors: {1}", tempList.size(), logErrors);
     }
@@ -62,15 +76,52 @@ final class FreeConnectionBuffer {
    */
   int trim(int minSize, long usedSince, long createdSince) {
     int trimCount = 0;
-    ListIterator<PooledConnection> iterator = freeBuffer.listIterator(minSize);
-    while (iterator.hasNext()) {
-      PooledConnection pooledConnection = iterator.next();
-      if (pooledConnection.shouldTrim(usedSince, createdSince)) {
-        iterator.remove();
-        pooledConnection.closeConnectionFully(true);
+    Node trimFrom = free.next;
+    while (minSize-- > 0) {
+      if (trimFrom.isEgeNode()) {
+        return 0;
+      }
+      trimFrom = trimFrom.next;
+    }
+
+    while (!trimFrom.isEgeNode()) {
+
+      if (trimFrom.pc.shouldTrim(usedSince, createdSince)) {
+        trimFrom.remove();
+        size--;
+        trimFrom.pc.closeConnectionFully(true);
         trimCount++;
       }
+      trimFrom = trimFrom.next;
     }
     return trimCount;
+  }
+
+  static class Node {
+
+    Node next;
+    Node prev;
+    final PooledConnection pc;
+
+    Node(PooledConnection pc) {
+      this.pc = pc;
+    }
+
+    boolean isEgeNode() {
+      return pc == null;
+    }
+
+    public void add(Node node) {
+      node.next = next;
+      node.prev = this;
+      next.prev = node;
+      next = node;
+    }
+
+    public void remove() {
+      assert pc != null : "called remove on an edge node";
+      next.prev = prev;
+      prev.next = next;
+    }
   }
 }
